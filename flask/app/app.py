1b1c5f05bb05e58a2768d7ec3264372db7b245e1
Ruslan <66064451+DarkPrinceWarrior@users.noreply.github.com>
2022-06-10 15:42:03 +0300

    added data as JSON

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,14 +1,32 @@
-from flask import render_template
+from flask import render_template, request, jsonify
 from flask.app import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import SelectField
+
 from models.db_setup import db_session
 from models.entities import Role
+
+import os
+
+SECRET_KEY = os.urandom(32)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5432/gen_Imagedb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JSON_AS_ASCII'] = False
+app.config['SECRET_KEY'] = SECRET_KEY
 db = SQLAlchemy(app)
+
+
+class Form(FlaskForm):
+    sex = SelectField('sex', choices=["man", "woman"])
+    hair = SelectField('hair', choices=["long", "short"])
+    eyes = SelectField('eyes', choices=["big", "small"])
+    smile = SelectField('smile', choices=["normal", "happy", "sad"])
+    face = SelectField('face', choices=["oval", "square", "round"])
+    age = SelectField('age', choices=["young", "middle", "old"])
+    nose = SelectField('nose', choices=["big", "small"])
 
 
 @app.teardown_appcontext
@@ -16,10 +34,16 @@ def shutdown_session(exception=None):
     db_session.remove()
 
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    form = Form()
     roles = db_session.query(Role).all()
-    return render_template('index.html', roles=roles)
+
+    if request.method == "POST":
+        data = {key: value for key, value in form.data.items() if key != "csrf_token"}
+        return jsonify(data)
+
+    return render_template('index.html', roles=roles, form=form)
 
 
 if __name__ == '__main__':
