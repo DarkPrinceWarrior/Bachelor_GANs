16b95f8f3b8c47b87a3a952357127e5396f13eb3
Ruslan <66064451+DarkPrinceWarrior@users.noreply.github.com>
2022-06-12 17:17:28 +0300

    nn structure

--- flask/app/app.py
+++ flask/app/app.py
@@ -1,8 +1,12 @@
+import numpy as np
+import torch
 from flask import render_template, request, jsonify
 from flask.app import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from tensorflow import Variable
 from wtforms import SelectField
+from pytorch2keras.converter import pytorch_to_keras
 
 from models.db_setup import db_session
 from models.entities import Role
@@ -52,5 +56,11 @@ def home():
 
 
 if __name__ == '__main__':
+    # path = "../app/NN_module/model/image_encoder550.pth"
+    # model = torch.load(path, map_location=torch.device("cuda"))
+    # input_np = np.random.uniform(0, 1, (1, 10, 32, 32))
+    # input_var = Variable(torch.FloatTensor(input_np))
+    # k_model = pytorch_to_keras(model, input_var, [(10, None, None,)], verbose=True)
+
     # models.db_setup.init_db()
     app.run(host="0.0.0.0", debug=True)
